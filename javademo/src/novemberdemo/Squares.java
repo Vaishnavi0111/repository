9cb2a52ad343233ef7eac05e3d654c188c5b2dce
Vaishnavi0111 <57611339+Vaishnavi0111@users.noreply.github.com>
2022-11-27 23:10:53 +0530

    Update Squares.java

--- javademo/src/novemberdemo/Squares.java
+++ javademo/src/novemberdemo/Squares.java
@@ -2,7 +2,7 @@ package novemberdemo;
 
 public class Squares extends Thread{
 
-	public static void 	Squares() throws InterruptedException {
+	public static void Squares() throws InterruptedException {
 		for(int i=0;i<10;i++) {
 			System.out.println("Squares of i is: "+Math.pow(i, 2));
 			Thread.sleep(10);
